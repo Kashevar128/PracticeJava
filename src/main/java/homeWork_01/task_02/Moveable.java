ec47fc7f9caea91cc6af981c6416664562583b54
mstislav <steve128@yandex.ru>
2023-01-23 19:43:14 +0300

    revert commit 2

--- src/main/java/homeWork_01/task_02/Moveable.java
+++
@@ -1,5 +0,0 @@
-package homeWork_01.task_02;
-
-public interface Moveable {
-    void move();
-}
--- src/main/java/homeWork_01/task_03/figure/Portray.java
+++
@@ -1,6 +0,0 @@
-package homeWork_01.task_03.figure;
-
-public interface Portray {
-
-    void draw();
-}

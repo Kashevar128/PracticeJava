ada781f49bf2d5f95eb6dd97abf4ad75fb8753f3
mstislav <steve128@yandex.ru>
2023-01-24 16:49:45 +0300

    add HW_02

---
+++ src/main/java/homeWork_02/linkedList/MyLinkedList.java
@@ -0,0 +1,216 @@
+package homeWork_02.linkedList;
+
+import java.util.NoSuchElementException;
+
+public class MyLinkedList<T>  {
+    private Node first;
+    private Node last;
+
+    private int size;
+
+    private class Node {
+        Node prev;
+        T value;
+        Node next;
+
+        public Node(Node prev, T value, Node next) {
+            this.prev = prev;
+            this.value = value;
+            this.next = next;
+        }
+
+        public Node(T value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+
+        public Node(T value) {
+            this.value = value;
+        }
+
+        public T getValue() {
+            return value;
+        }
+
+        public void setValue(T value) {
+            this.value = value;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+        public Node getPrev() {
+            return prev;
+        }
+
+        public void setPrev(Node prev) {
+            this.prev = prev;
+        }
+    }
+
+    public void addFirst(T item) {
+        Node newNode = new Node(item, first);
+        if (isEmpty()) {
+            last = newNode;
+        } else {
+            first.setPrev(newNode);
+        }
+        first = newNode;
+        size++;
+    }
+
+    public T removeFirst() {
+        T temp = getFirst();
+        first = first.getNext();
+        if (isEmpty()) {
+            last = null;
+        } else {
+            first.setPrev(null);
+        }
+        size--;
+        return temp;
+    }
+
+    public void addLast(T item) {
+        Node newNode = new Node(item);
+        if (isEmpty()) {
+            first = newNode;
+        } else {
+            newNode.setPrev(last);
+            last.setNext(newNode);
+        }
+        last = newNode;
+        size++;
+    }
+
+    public T removeLast() {
+        T temp = getLast();
+        if (last.getPrev() == null) {
+            first = null;
+        } else {
+            last.getPrev().setNext(null);
+        }
+        last = last.getPrev();
+        size--;
+        return temp;
+    }
+
+    private T getFirst() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Empty");
+        }
+        return first.value;
+    }
+
+    private T getLast() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Empty");
+        }
+        return last.value;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public final int indexOf(T item) {
+        Node current = first;
+        int index = 0;
+        while (current != null) {
+            if (current.value.equals(item)) {
+                return index;
+            }
+            current = current.getNext();
+            index++;
+        }
+        return -1;
+    }
+
+    public boolean contains(T item) {
+        return indexOf(item) > -1;
+    }
+
+    public void add(int index, T item) {
+        if (index < 0 || index > size) {
+            throw new IllegalStateException("index: " + index);
+        }
+        if (index == 0) {
+            addFirst(item);
+            return;
+        }
+        if (index == size) {
+            addLast(item);
+            return;
+        }
+
+        Node current = first;
+        int i = 0;
+        while (i < index - 1) {
+            current = current.getNext();
+            i++;
+        }
+        Node newNode = new Node(current, item, current.getNext());
+        current.getNext().setPrev(newNode);
+        current.setNext(newNode);
+        size++;
+    }
+
+    public boolean remove(T item) {
+        if (isEmpty()) {
+            return false;
+        }
+        if (first.value.equals(item)) {
+            removeFirst();
+            return true;
+        }
+        if (last.value.equals(item)) {
+            removeLast();
+            return true;
+        }
+
+        Node current = first;
+        while (current != null && !current.getValue().equals(item)) {
+            current = current.getNext();
+        }
+        if (current == null) {
+            return false;
+        }
+        if (current == first) {
+            removeFirst();
+            return true;
+        }
+        if (current == last) {
+            removeLast();
+            return true;
+        }
+
+        current.getPrev().setNext(current.getNext());
+        current.getNext().setPrev(current.getPrev());
+        size--;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+        Node current = first;
+        while (current != null) {
+            sb.append(current.value).append(", ");
+            current = current.getNext();
+        }
+        if (!isEmpty()) {
+            sb.setLength(sb.length() - 2);
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+}
